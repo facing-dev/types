d78ed0425fd47d479364ef6b0df1e68bc581b0fe
li lei <ruojianll@gmail.com>
2024-02-25 20:48:01 +0800

    lint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+const config = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig/tsconfig.esm.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    }
+  },
+  {
+    ignores: [
+      "src/**/test.ts",
+      "src/**/*.test.ts"
+    ]
+  }
+);
+
+export default config
